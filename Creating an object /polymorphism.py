b0ef991988f4bf6ff1c6bdc94d18dee831175c18
Sandoval <sandovalbgasantiago@gmail.com>
2024-05-06 17:00:33 -0500

    sano

---
+++ Creating an object /polymorphism.py
@@ -0,0 +1,3 @@
+# Polymorphism concept refers to the feature that allows different classes to use the same method, but with different results.
+
+# We are working above the same project, so, to use this OOP property, we going to establish 
